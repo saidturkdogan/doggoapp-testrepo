c3952b41c695d86a778685dd18bbee90df8afb77
Said Cemal Türkdoğan <69085609+saidturkdogan@users.noreply.github.com>
2024-09-10 01:23:56 +0300

    Commit Message

--- src/test/java/WalkerBasvuruTest.java
+++ src/test/java/WalkerBasvuruTest.java
@@ -19,7 +19,7 @@ public class WalkerBasvuruTest {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--window-size=1920,1080");
@@ -28,52 +28,54 @@ public class WalkerBasvuruTest {
         driver.get("https://devweb.doggoapp.com/walker-basvuru");
         System.out.println("Driver başarıyla başlatıldı ve sayfa yüklendi.");
     }
+
+    private WebDriverWait getWait() {
+        if (this.wait == null) {
+            this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        }
+        return this.wait;
+    }
+
     @Test
     public void testWalkerBasvuru(){
-
-        WebElement input_firstName = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("firstName")));
+        WebElement input_firstName = getWait().until(ExpectedConditions.presenceOfElementLocated(By.name("firstName")));
         input_firstName.sendKeys("John");
         System.out.println("İsim girildi.");
 
-        WebElement input_lastName = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("lastName")));
+        WebElement input_lastName = getWait().until(ExpectedConditions.presenceOfElementLocated(By.name("lastName")));
         input_lastName.sendKeys("Doe");
         System.out.println("Soyad girildi.");
 
-        WebElement input_phone = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("telephone")));
+        WebElement input_phone = getWait().until(ExpectedConditions.presenceOfElementLocated(By.name("telephone")));
         input_phone.sendKeys("5397240935");
         System.out.println("Telefon numarası girildi.");
 
-        WebElement email = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
+        WebElement email = getWait().until(ExpectedConditions.presenceOfElementLocated(By.name("email")));
         email.sendKeys("asd@gmail.com");
         System.out.println("E-posta adresi girildi.");
 
-        WebElement password = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("password")));
+        WebElement password = getWait().until(ExpectedConditions.presenceOfElementLocated(By.name("password")));
         password.sendKeys("turkdogan2525.");
         System.out.println("Şifre girildi.");
 
-        WebElement repeatPassword = wait.until(ExpectedConditions.presenceOfElementLocated(By.name("repeatPassword")));
+        WebElement repeatPassword = getWait().until(ExpectedConditions.presenceOfElementLocated(By.name("repeatPassword")));
         repeatPassword.sendKeys("turkdogan2525.");
         System.out.println("Şifre tekrar girildi.");
 
-        WebElement dropdownProvince = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/main/div/div/section/form/div[1]/div[10]/div[2]/button/div")));
+        WebElement dropdownProvince = getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/main/div/div/section/form/div[1]/div[10]/div[2]/button/div")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", dropdownProvince);
         System.out.println("İl dropdown'ı tıklandı.");
 
-        dropdownProvince = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='İstanbul']")));
+        dropdownProvince = getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='İstanbul']")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", dropdownProvince);
         System.out.println("İstanbul' seçeneği seçildi.");
 
-        WebElement dropdownProvinceILCE = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/main/div/div/section/form/div[1]/div[11]/div[2]/button/div")));
+        WebElement dropdownProvinceILCE = getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div/main/div/div/section/form/div[1]/div[11]/div[2]/button/div")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", dropdownProvinceILCE);
         System.out.println("İlçe dropdown'ı tıklandı.");
 
-        dropdownProvinceILCE = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Kadıköy']")));
+        dropdownProvinceILCE = getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Kadıköy']")));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", dropdownProvinceILCE);
         System.out.println("Kadıköy' seçeneği seçildi.");
     }
-
-
-
-
-
 }
